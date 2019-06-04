c8f350ace7c55d0f750c6225924977f65c59a5c7
Alan Krajina <alankrajina88@gmail.com>
2019-06-04 12:51:32 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,15 +1,20 @@
 require 'pry'
 require "nokogiri"
 
-html = File.read('fixtures/kickstarter.html')
-kickstarter = Nokogiri::HTML(html)
-
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+ 
+  projects = {}
+ 
+  # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+ 
+  # return the projects hash
+  projects
 end
-binding.pry
-
-create_project_hash
 
 
 # projects: kickstarter.css("li.project.grid_4")  (svi projekti)
